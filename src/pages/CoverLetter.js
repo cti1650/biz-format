d8e283568857905ce83ae6c0f8bb71978f58b622
cti1650 <cti1650@gmail.com>
2021-02-22 23:04:46 +0900

    test

---
+++ src/pages/CoverLetter.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from "gatsby"
+import PageA4 from "../components/page"
+
+export default () => (
+  <>
+    <PageA4 />
+  </>
+)
--- src/pages/second.js
+++ src/pages/second.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby"
 import PageA4 from "../components/page"
 
 export default () => (
-  <PageA4>
-    <div>
-      <p>2つ目のページのメインコンテンツ！</p>
-      <Link to={"/"}>トップページへのリンク</Link>
-    </div>
-  </PageA4>
+  <>
+    <PageA4>
+      <div>
+        <p>2つ目のページのメインコンテンツ！</p>
+        <Link to={"/"}>トップページへのリンク</Link>
+      </div>
+    </PageA4>
+  </>
 )
