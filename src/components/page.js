1fa950c3456db5473cb268f627be22a587a4ff9b
cti1650 <cti1650@gmail.com>
2021-02-22 22:00:07 +0900

    test

--- src/components/page.js
+++ src/components/page.js
@@ -28,10 +28,10 @@ class PageA4 extends React.Component {
         {" "}
         {/* <React.Fragment>の省略形 */}
         <section className={Styles.pageA4}>
-          <p className="border-black border-l-8 border-b my-6 py-1 pl-4 h-auto">
+          <p tw="border-black border-l-8 border-b my-6 py-1 pl-4 h-auto">
             <input
               type="text"
-              className="text-xl text-left py-1 tracking-widest border-white border-0 w-full"
+              tw="text-xl text-left py-1 tracking-widest border-white border-0 w-full"
               value="書類送付のご案内"
             />
           </p>
@@ -41,36 +41,40 @@ class PageA4 extends React.Component {
           <p className="text-sm p-0 text-left">
             <input
               type="text"
-              className="syncdata text-lg border-white border-0 underline w-full"
+              className="syncdata"
+              tw="text-lg border-white border-0 underline w-full"
               id="to_Name"
               value="Example 御中"
             />
           </p>
-          <p className="text-sm text-left h-auto">
+          <p tw="text-sm text-left h-auto">
             <textarea
-              className="syncdata text-sm text-left break-words border-white border-0 w-full h-full"
+              className="syncdata"
+              tw="text-sm text-left break-words border-white border-0 w-full h-full"
               id="to_Address"
             >
               東京都○○区○○1-2-3 ○○　○○ 000-0000-0000
             </textarea>
           </p>
-          <p className="text-lg p-0 text-right">
+          <p tw="text-lg p-0 text-right">
             <input
               type="text"
-              className="syncdata text-lg text-right border-white border-0 w-full"
+              className="syncdata"
+              tw="text-lg text-right border-white border-0 w-full"
               id="from_Name"
               value="Example Example"
             />
           </p>
-          <p className="text-sm text-right h-auto">
+          <p tw="text-sm text-right h-auto">
             <textarea
-              className="syncdata text-sm text-right break-words border-white border-0 w-full h-full"
+              className="syncdata"
+              tw="text-sm text-right break-words border-white border-0 w-full h-full"
               id="from_Address"
             >
               東京都○○区○○1-2-3 ○○　○○ 00-0000-0000
             </textarea>
           </p>
-          <p className="text-left break-words mt-8">
+          <p tw="text-left break-words mt-8">
             拝啓　時下ますますご清栄のこととお喜び申し上げます。平素は格別のお引き立てを賜り
             <br />
             誠にありがとうございます。
@@ -80,81 +84,81 @@ class PageA4 extends React.Component {
             <br />
             まずは、書類送付のご案内まで。
           </p>
-          <p className="text-right">敬具</p>
-          <p className="text-center my-8">記</p>
-          <ol className="border-2 border-black border-solid divide-y-2 divide-gray-400 divide-dotted p-0 my-6">
+          <p tw="text-right">敬具</p>
+          <p tw="text-center my-8">記</p>
+          <ol tw="border-2 border-black border-solid divide-y-2 divide-gray-400 divide-dotted p-0 my-6">
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
             <li>
               <input
                 type="text"
-                className="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
+                tw="text-xl text-left py-1 px-4 tracking-widest border-white border-0 w-full"
                 value=""
               />
             </li>
           </ol>
-          <p className="text-right">以上</p>
+          <p tw="text-right">以上</p>
         </section>
       </>
     )
